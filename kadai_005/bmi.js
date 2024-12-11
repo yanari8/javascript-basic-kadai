d1b5414578cce593e60692f620eb68eda2ab3b6e
yanari8 <yanari8co@gmail.com>
2024-12-11 12:44:54 +0900

    create kadai_005

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+// 変数
+// 体重　単位kg
+let bodyWeight;
+// 身長　　単位m
+let bodyHeight;
+// BMI
+let bodyBmi;
+
+// 値の代入
+bodyWeight = 68;
+bodyHeight = 1.7;
+
+// BMI計算式
+bodyBmi = bodyWeight / (bodyHeight * bodyHeight);
+
+// 出力
+console.log(bodyBmi);
